6e00ae7de57646b829d3982c8d25368994612980
Maxim Gunter <gunter.maxim@gmail.com>
2018-10-24 00:42:21 -0400

    Added filters to ToolBar

--- deckofcards/src/app/app.component.ts
+++ deckofcards/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { dropDown } from './dropDown';
+import { Filter } from './models/filter';
 
 @Component({
   selector: 'zipari-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'zipari';
+
+  selectedMinValue: number;
+  selectedMaxValue: number;
+  selectedNumberOfCards: number;
+
+  filters : Filter[] = [
+    {name: "Spade", selected: false},
+    {name: "Heart", selected: false},
+    {name: "Club", selected: false},
+    {name: "Diamond", selected: false}
+  ];
+
+  cards: dropDown[] = [
+    {value: '2', viewValue: '2'},
+    {value: '3', viewValue: '3'},
+    {value: '4', viewValue: '4'},
+    {value: '5', viewValue: '5'},
+    {value: '6', viewValue: '6'},
+    {value: '7', viewValue: '7'},
+    {value: '8', viewValue: '8'},
+    {value: '9', viewValue: '9'},
+    {value: '10', viewValue: '10'},
+    {value: '11', viewValue: 'Jack'},
+    {value: '12', viewValue: 'Queen'},
+    {value: '13', viewValue: 'King'},
+    {value: '14', viewValue: 'Ace'},
+
+  ];
 }
+
+
--- deckofcards/src/app/app.module.ts
+++ deckofcards/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './shared/material/material.module';
@@ -10,7 +12,10 @@ import { MaterialModule } from './shared/material/material.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
